746b157faed31d6f8d5972a6507c4d6c0b81e5a2
hahaa <concac@gmail.com>
2024-09-14 20:06:10 +0700

    haha

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import torch
-from model import Diffusion_Video_Model
+# from model import Diffusion_Video_Model
 
-torch.set_printoptions(
-    precision = 16,
-    sci_mode = False,
-    threshold = 100
-)
+# torch.set_printoptions(
+#     precision = 16,
+#     sci_mode = False,
+#     threshold = 100
+# )
 
-model = Diffusion_Video_Model()
-if torch.cuda.is_available():
-    model.cuda()
+# model = Diffusion_Video_Model()
+# if torch.cuda.is_available():
+#     model.cuda()
 
-model.infer([
-    "I eat shit",
-    "I love you"
-], (64, 96))
+# model.infer([
+#     "I eat shit",
+#     "I love you"
+# ], (64, 96))
+
+i = 5
+
+for j in range(2, 9):
+    a, i = tuple((i * 2, i * 3))
+
+print(i)
