e09e6c093b22ccec758cad0ba1e2df3c8d4f2dc7
hahaa <concac@gmail.com>
2024-09-22 10:38:57 +0700

    haha

--- model_low_gpu.py
+++ model_low_gpu.py
@@ -564,7 +564,8 @@ class Diffusion_Video_Model(nn.Module):
     def train_auto_encoder(self, time_step):
         resolution = time_step % 6
         if resolution == 0:
-            resolution = [512, 384]
+            # change this shit to 512 384
+            resolution = [256, 256]
         elif resolution == 1:
             resolution = [768, 512]
         elif resolution == 2:
