04c760472a448baa104a11045bc55b897fadf850
hahaa <concac@gmail.com>
2024-09-22 12:36:19 +0700

    haha

--- function_low_gpu.py
+++ function_low_gpu.py
@@ -4,6 +4,8 @@ import string
 import random
 import os
 
+random.seed(0)
+
 def generate_tensor_file_name():
     while True:
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k = 10))
@@ -16,6 +18,7 @@ class One_Input_Call(torch.autograd.Function):
     def forward(context, module, input_tensor):
         context.module = module.net
         context.name = os.path.join("computational_graph", generate_tensor_file_name())
+        print("forward : " + context.name)
         torch.save(input_tensor, context.name)
         torch.cuda.empty_cache()
         return module.net(input_tensor)
@@ -23,6 +26,7 @@ class One_Input_Call(torch.autograd.Function):
     def backward(context, output_gradient):
         input_tensor = torch.load(context.name, weights_only = True)
         os.remove(context.name)
+        print("backward : " + context.name)
         with torch.enable_grad():
             output_tensor = context.module(input_tensor)
             output_tensor.backward(output_gradient)
@@ -39,6 +43,11 @@ class Three_Input_Call(torch.autograd.Function):
         torch.save(input_tensor_2, context.name_2)
         context.name_3 = os.path.join("computational_graph", generate_tensor_file_name())
         torch.save(input_tensor_3, context.name_3)
+
+        print("forward : " + context.name_1)
+        print("forward : " + context.name_2)
+        print("forward : " + context.name_3)
+
         torch.cuda.empty_cache()
         return module.net(input_tensor_1, input_tensor_2, input_tensor_3)[0]
         
@@ -46,14 +55,12 @@ class Three_Input_Call(torch.autograd.Function):
         input_tensor_1 = torch.load(context.name_1, weights_only = True)
         input_tensor_2 = torch.load(context.name_2, weights_only = True)
         input_tensor_3 = torch.load(context.name_3, weights_only = True)
-        print(context.name_1)
-        print(context.name_2)
-        print(context.name_3)
         os.remove(context.name_1)
         os.remove(context.name_2)
         os.remove(context.name_3)
-        print("succ")
-        a = None * 2
+        print("backward : " + context.name_1)
+        print("backward : " + context.name_2)
+        print("backward : " + context.name_3)
         with torch.enable_grad():
             output_tensor = context.module(input_tensor_1, input_tensor_2, input_tensor_3)[0]
             output_tensor.backward(output_gradient)
